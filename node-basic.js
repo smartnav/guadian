ff7d7c24756fdb2c7c47d81938023b5063771a64
Shiven Juneja <smartdata.nav@gmail.com>
2016-06-09 18:00:56 +0530

    mail on error

--- node-basic.js
+++ node-basic.js
@@ -3,8 +3,16 @@ var render = require('koa-ejs');
 var path = require('path');
 var logger = require('koa-logger')
 var bodyParser = require('koa-bodyparser');
-
-
+const transporter = require('nodemailer').createTransport({
+	host: 'smtp.gmail.com',
+	port: 465,
+	secure: true, // use SSL
+	auth: {
+		user: process.env.GMAIL_USER,
+		pass: process.env.GMAIL_PASSWORD
+	}
+});
+const URL = require('./config/url');
 var app = koa();
 app.use(logger())
 app.use(bodyParser());
@@ -19,6 +27,26 @@ render(app, {
   debug: true
 });
 
+app.use(function *(next) {
+  try {
+    // yield downstream
+    yield next;
+  } catch (err) {
+    this.status = err.status || 500;
+    this.body = err.message;
+    transporter.sendMail({
+            from: `<no-reply@${URL}>`,
+            to: "shiven.juneja@gmail.com",
+            subject: 'Error '+this.status,
+            text: err.stack
+          }, function(error, info) {
+            if(error) {
+              return console.error(error);
+            }
+            console.log('Email sent', info);
+          });
+  }
+});
 
 require('./app/routes.js')(app);
 //routes
