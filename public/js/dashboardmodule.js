bb3739b4fb1529ba1fdac751014968330998fafd
Shiven Juneja <smartdata.nav@gmail.com>
2016-06-15 17:15:40 +0530

    changes in confirm box

--- public/js/dashboardmodule.js
+++ public/js/dashboardmodule.js
@@ -331,17 +331,3 @@ $scope.hover = function(data) {
             
  }
 })
-.directive('ngConfirmClick', [
-        function(){
-            return {
-                link: function (scope, element, attr) {
-                    var msg = attr.ngConfirmClick || "Are you sure?";
-                    var clickAction = attr.confirmedClick;
-                    element.bind('click',function (event) {
-                        if ( window.confirm(msg) ) {
-                            scope.$eval(clickAction)
-                        }
-                    });
-                }
-            };
-    }])
