44a932da1c21175e31a8936b3d81b5fc6c8c8da4
Shiven Juneja <smartdata.nav@gmail.com>
2016-06-27 12:08:57 +0530

    todays task

--- public/js/sharemodule.js
+++ public/js/sharemodule.js
@@ -1,7 +1,20 @@
 angular
-.module('share',[])
-.controller('ShareLink',function($scope,$http){
-	
+.module('share',['toaster', 'ngAnimate','ngclipboard'])
+.controller('ShareLink',function($scope,$http,toaster){
+	$scope.onSuccess = function(e) {
+    toaster.pop('success', "Success", 'Copied Successfully');
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+};
+
+$scope.onError = function(e) {
+    toaster.pop('error', "Error", 'Error copying text.');
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+}
 	$scope.forms = {};
 	$scope.formid=0;
     $scope.linkurl = window.location.href.replace('share','');
@@ -9,10 +22,18 @@ angular
 	$scope.getForms = function() {
 
     		$http.post('/usergroup/getgroups',{offset:1,limit:10000}).then(function(response){
+            if(response.data.result.length>0) { 
+                $scope.forms = response.data.result;
+                $scope.formid = $scope.forms[0].id;
+                $scope.makeLink();
+            }
+    		
+            else
+           {
+            $scope.forms.push({facility:"Select Form",id:0});
+           }
+           })
 
-    		$scope.forms = response.data.result
-    		})
-    	
     }
 
     $scope.getForms();
